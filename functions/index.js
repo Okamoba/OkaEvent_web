cebc7493efc11204015acf8e5fe658ee1b3872b5
keiichilo <keiichiloooo@gmail.com>
2018-02-15 19:41:11 +0900

    create function makeNewEvent

--- functions/index.js
+++ functions/index.js
@@ -10,14 +10,39 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+exports.makeNewEvent = functions.database.ref('/tmp/event/{uId}')
     .onWrite(event => {
-      // Grab the current value of what was written to the Realtime Database.
-      const original = event.data.val();
-      console.log('Uppercasing', event.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return event.data.ref.parent.child('uppercase').set(uppercase);
+      // get event_ID
+      var event_id = 0;
+      if (!event.data.child('event_id').exists()) {
+        admin.database().ref('events/id').once('value').then(function(snapshot) {
+          event_id = snapshot.val();
+          event.data.ref.child('event_id').set(event_id);
+          admin.database().ref('events/id').set(event_id + 1);
+        });
+        return 2;
+      } else {
+        event_id = event.data.child('event_id').val();
+      }
+
+      // set values
+      if (event.data.hasChildren()) {
+        var isUpdated = false;
+
+        event.data.forEach((eventSnap) => {
+          if (eventSnap.exists()) {
+            if (eventSnap.key == 'event_id') {
+              return;
+            }
+            isUpdated = true;
+            const new_val = eventSnap.val();
+            console.log('set ' + eventSnap.key, event.params.uId, new_val);
+            admin.database().ref('events/' + event_id).child(eventSnap.key).set(new_val);
+          }
+        });
+        if (isUpdated) {
+          admin.database().ref('events/' + event_id).child('author').set(event.params.uId);
+        }
+      }
+      return 1;
     });
